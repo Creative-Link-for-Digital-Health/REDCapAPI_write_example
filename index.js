879bb4436d3df344cb88086798d20363feead392
Dmitry Strakovsky <shifting.planes@gmail.com>
2024-05-01 09:12:46 -0400

    updated port info

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 3333
+const port = process.env.PORT || 3333
 
 app.get('/testing', (req, res) => {
   res.json({"test":"success"})
 })
 
+app.get('/trigger', (req, res) => {
+  res.json({"test":"success"})
+})
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
