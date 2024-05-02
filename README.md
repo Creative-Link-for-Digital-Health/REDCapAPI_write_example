32b34df6d300fa43cba8af82f3fe9b7ece9f1d7d
Dmitry Strakovsky <shifting.planes@gmail.com>
2024-05-01 22:25:46 -0400

    completed first pass through the python notebook and basic node server without write capability

---
+++ README.md
@@ -0,0 +1,3 @@
+# REDCap API Write Example
+
+A quick example Python notebook and Node server that write to REDCap API
--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 const express = require('express')
 const app = express()
+
+const { 
+  v1: uuidv1,
+  v4: uuidv4,
+} = require('uuid')
+
+
 const port = process.env.PORT || 3333
 
-app.get('/testing', (req, res) => {
-  res.json({"test":"success"})
+
+const GenerateTestREDCapAPIPostData = () => {
+
+  let send_data = '[{"record_id":"'+ uuidv1() + '", "user_id":"1234B"}]'
+  
+  post_data = {
+    'token': 'C1A72302B7004CAC79A1769811ED0429',
+    'content': 'record',
+    'action': 'import',
+    'format': 'json',
+    'type': 'flat',
+    'overwriteBehavior': 'normal',
+    'forceAutoNumber': 'false',
+    'data': send_data ,
+    'returnContent': 'count',
+    'returnFormat': 'json'
+  }
+
+  return 
+}
+
+app.get('/ping', (req, res) => {
+  res.json({"ping":"success"})
 })
 
-app.get('/trigger', (req, res) => {
+app.get('/write', (req, res) => {
+
+  console.log(GenerateTestREDCapAPIPostData())
+
   res.json({"test":"success"})
 })
 
