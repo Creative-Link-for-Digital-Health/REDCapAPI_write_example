fc7695d5a089b84f6a1fc9153ac1d670db0183b7
Dmitry Strakovsky <shifting.planes@gmail.com>
2024-05-05 19:35:38 -0400

    Got Node server working.

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # REDCap API Write Example
 
-A quick example Python notebook and Node server that write to REDCap API
+A quick example Python notebook and Node server that write to REDCap API. 
+
+## To use
+Drop you API key and your institution's path to REDCap API endpoint into an .env file (generate .env if needed using .env.example)
+
+Run the Python notebook at REDCapWrite.ipynb 
+
+Run the Node server by running in terminal ```node index.js``` and then navigating to http://localhost:3333/ping to test if the server is up (you should get ```{"ping":"success"}``` back) and then http://localhost:3333/test-write to write to you REDCap project db.
+
+
+## Notes
+Make sure that the table you are writing to has *user_id* field or alter your data payload to reflect your data structures.   
+
+Each write request generates a unique id for the record, otherwise records will be overwritten. This is a bit brute force and assumes that you will be filtering data on other fields for the final data analysis.
--- index.js
+++ index.js
@@ -1,44 +1,85 @@
+require('dotenv').config()
+const axios = require('axios');
 const express = require('express')
 const app = express()
-
-const { 
-  v1: uuidv1,
-  v4: uuidv4,
-} = require('uuid')
-
+const {v1: uuidv1} = require('uuid')
+const url = require('url')
 
 const port = process.env.PORT || 3333
 
 
 const GenerateTestREDCapAPIPostData = () => {
 
-  let send_data = '[{"record_id":"'+ uuidv1() + '", "user_id":"1234B"}]'
+  let send_data = '[{"record_id":"'+ uuidv1() + '", "user_id":"Final Countdown"}]'
   
   post_data = {
-    'token': 'C1A72302B7004CAC79A1769811ED0429',
+    'token': process.env.REDCAP_API_KEY,
     'content': 'record',
     'action': 'import',
     'format': 'json',
     'type': 'flat',
     'overwriteBehavior': 'normal',
     'forceAutoNumber': 'false',
-    'data': send_data ,
+    'data': encodeURIComponent(send_data) ,
     'returnContent': 'count',
     'returnFormat': 'json'
   }
 
-  return 
+  return post_data
 }
+
+//console.log(GenerateTestREDCapAPIPostData()) 
+
+// Have to build a custom url encoding because REDCap only accepts data as 'Content-Type': 'application/x-www-form-urlencoded' and various tools are trying to escape uri-ecoded chars
+// i.e. hard to serialize data that is part uri encoded and part json :(
+
+const BuildParams = (data_object) => {
+  let stringified =""
+  for (let key in data_object) {
+    if (data_object.hasOwnProperty(key)) {
+      stringified += `${key}=${data_object[key]}&`
+    }
+  } 
+
+  return stringified.slice(0, -1)       //kill the last trailing &
+}
+
+console.log(BuildParams(GenerateTestREDCapAPIPostData()))
+
+
+
 
 app.get('/ping', (req, res) => {
   res.json({"ping":"success"})
 })
 
-app.get('/write', (req, res) => {
+app.get('/test-write', (req, res) => {
 
-  console.log(GenerateTestREDCapAPIPostData())
+  console.log("start write")
 
-  res.json({"test":"success"})
+  const params = BuildParams(GenerateTestREDCapAPIPostData())
+
+  console.log("built params")
+
+  const options = {
+    method: 'POST',
+    headers: { 'content-type': 'application/x-www-form-urlencoded' },
+    data: params,
+    url: process.env.REDCAP_API_ENDPOINT,
+  }
+  
+  console.log("started the call to REDCap")
+
+axios(options)
+  .then(function (response) {
+    res.json({"test":"success", "payload": response.data})
+    // res.json({"test":"success"})
+    // console.log(response);
+  })
+  .catch(function (error) {
+    res.json({"test":"error", "payload": error})
+    // console.log(error);
+  })
 })
 
 app.listen(port, () => {
